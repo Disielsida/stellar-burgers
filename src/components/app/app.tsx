d2f0530ae79f3d0bde112d7cc0c1481d343ac2ab
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-09-23 10:32:17 +0300

    feat(router): add base routing with pages and modal routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { ROUTES } from '@utils-routes';
 import {
   ConstructorPage,
@@ -16,28 +16,57 @@ import styles from './app.module.css';
 
 import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
+const App = () => {
+  const navigate = useNavigate();
 
-    <Routes>
-      <Route path={ROUTES.HOME} element={<ConstructorPage />} />
-      <Route path={ROUTES.FEED} element={<Feed />} />
-      <Route path={ROUTES.LOGIN} element={<Login />} />
-      <Route path={ROUTES.REGISTER} element={<Register />} />
-      <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
-      <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
-      <Route path={ROUTES.PROFILE} element={<Profile />} />
-      <Route path={ROUTES.PROFILE_ORDERS} element={<ProfileOrders />} />
-      <Route path={ROUTES.NOT_FOUND} element={<NotFound404 />} />
-    </Routes>
+  const handleBackFromModal = () => {
+    navigate(-1);
+  };
 
-    <Routes>
-      <Route path={ROUTES.FEED_ORDER} />
-      <Route path={ROUTES.PROFILE_ORDER} />
-      <Route path={ROUTES.INGRIDIENT} />
-    </Routes>
-  </div>
-);
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+
+      <Routes>
+        <Route path={ROUTES.HOME} element={<ConstructorPage />} />
+        <Route path={ROUTES.FEED} element={<Feed />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.REGISTER} element={<Register />} />
+        <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
+        <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
+        <Route path={ROUTES.PROFILE} element={<Profile />} />
+        <Route path={ROUTES.PROFILE_ORDERS} element={<ProfileOrders />} />
+        <Route path={ROUTES.NOT_FOUND} element={<NotFound404 />} />
+      </Routes>
+
+      <Routes>
+        <Route
+          path={ROUTES.FEED_ORDER}
+          element={
+            <Modal title='' onClose={handleBackFromModal}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path={ROUTES.PROFILE_ORDER}
+          element={
+            <Modal title='' onClose={handleBackFromModal}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path={ROUTES.INGRIDIENT}
+          element={
+            <Modal title='Детали ингредиента' onClose={handleBackFromModal}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
